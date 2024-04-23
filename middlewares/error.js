432d99fc89abb7e2f10c7427a2ba6bc17f4e0141
Swarnab Banerjee <116362013+swarnab007@users.noreply.github.com>
2024-04-23 07:34:38 +0530

    Login Feature created

--- middlewares/error.js
+++ middlewares/error.js
@@ -5,4 +5,14 @@ class ErrorHandler extends Error {
   }
 }
 
+export const errorMiddleware = (err, req, res, next) => {
+  err.message = err.message || "Internal Server Error";
+  err.statusCode = err.statusCode || 500;
+
+  return res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
+
 export { ErrorHandler };
