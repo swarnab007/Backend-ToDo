918e50895dacb0d0e931a3dfda791df85e2a6bda
Swarnab Banerjee <116362013+swarnab007@users.noreply.github.com>
2024-05-01 12:44:35 +0530

    user controller cokie send function modified

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,12 +2,15 @@ import { User } from "../models/user.model.js";
 import { ErrorHandler } from "../middlewares/error.js";
 import bcrypt from "bcrypt";
 import { sendCookie } from "../utils/features.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res, next) => {
   console.log(req.body);
   try {
     const { username, email, fullname, password } = req.body;
     console.log(username, password);
+
+    if(!username || !email || !fullname || !password) return res.json(new ErrorHandler("All fields are required", 400));  
     if (
       [username, email, fullname, password].some(
         (field) => field?.trim() === ""
@@ -27,7 +30,31 @@ export const register = async (req, res, next) => {
       email,
       password: hashedPass,
     });
-    return sendCookie(user, res, "Registered successfully", 201);
+
+    const payload = {
+      email: user.email,
+      id: user._id,
+    };
+
+    let token = jwt.sign(payload, "djsjgjohwjhnwigbnmfmag353553hj", {
+      expiresIn: "2h",
+    });
+
+    const options = {
+      expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+    };
+
+    res
+    .status(200)
+    .cookie("token", token, options)
+    .json({
+      success: true,
+      message:`Registered successfully`,
+    });
+   // return sendCookie(user, res, "Registered successfully", 201);
+
+
   } catch (error) {
     console.log(error);
     next(error);
@@ -38,15 +65,46 @@ export const login = async (req, res) => {
   try {
     console.log(req.body);
     const { username, password } = req.body;
-    console.log(username);
+
+    if (!username || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "PLease fill all the details carefully",
+      });
+    }
+    
+    //check for registerd user
     let user = await User.findOne({username}).select("+password");
     
     if (!user) return res.send(new ErrorHandler("Register First", 400));
+
+
     const isMatchedPass = bcrypt.compare(password, user.password);
     if (!isMatchedPass)
       return res.send(new ErrorHandler("Wrong Password !", 400));
 
-    sendCookie(user, res, `Welcome Back ${user.fullname}`, 200);
+      const payload = {
+        email: user.email,
+        id: user._id,
+      };
+
+      let token = jwt.sign(payload, "djsjgjohwjhnwigbnmfmag353553hj", {
+        expiresIn: "2h",
+      });
+
+      const options = {
+        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+      };
+
+       res
+      .status(200)
+      .cookie("token", token, options)
+      .json({
+        success: true,
+        message:`Welcome Back ${user.fullname}`,
+      });
+      
   } catch (error) {
     console.log(error);
     // next(error);
