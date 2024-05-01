553c3ec28a59fbc7c82282dc882b461f6b6f833b
Swarnab Banerjee <116362013+swarnab007@users.noreply.github.com>
2024-05-01 09:46:19 +0530

    cors solved

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://frontend-to-do-mu.vercel.app/"],
+    origin: ["https://frontend-to-do-mu.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
