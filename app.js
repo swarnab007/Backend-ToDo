d6683de77eddb8a83980f8df4cfe9c8e08f537cd
Swarnab Banerjee <116362013+swarnab007@users.noreply.github.com>
2024-05-01 09:38:11 +0530

    cors error updated

--- app.js
+++ app.js
@@ -16,22 +16,14 @@ config({
 // using middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://frontend-to-do-mu.vercel.app/");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization,token"
-  );
-  res.header("Access-Control-Allow-Credentials", "true"); // Allow credentials
+app.use(
+  cors({
+    origin: ["https://frontend-to-do-mu.vercel.app/"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
-  // Handle preflight requests
-  if (req.method === "OPTIONS") {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
 
 // defining routes
 app.use("/api/v1/users", userRouter);
